ae48631ba67966dc6519ce7fdb9a4a871338b57e
gkiar <gkiar07@gmail.com>
2018-05-21 19:55:57 -0400

    updated templates and script

--- badges/makebadges.sh
+++ badges/makebadges.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p badges
+mkdir -p badges2
 
 IFS=$'\r\n'
 set -f
@@ -9,8 +9,9 @@ for line in $(cat < "$1");
 do
     echo $line | cut -d ',' -f 1
     cp ohbm_brainhack_2018_badge_front_template.svg tmp.svg
-    sed -i -e "s/{ParticipantName}/"`echo $line | cut -d ',' -f 1`"/g" tmp.svg
-    sed -i -e "s/{Affiliation}/"`echo $line | cut -d ',' -f 2`", "`echo $line | cut -d ',' -f 3`"/g" tmp.svg
-    sed -i -e "s/{PositionAtEvent}/"`echo $line | cut -d ',' -f 4`"/g" tmp.svg
-    rsvg-convert -f pdf -o "badges/ohbm_brainhack_2018_badge_front_"`echo $line | cut -d ',' -f 1`".pdf" tmp.svg
+    sed -i -e "s/{First}/"`echo $line | cut -d ',' -f 1`"/g" tmp.svg
+    sed -i -e "s/{Last}/"`echo $line | cut -d ',' -f 2`"/g" tmp.svg
+    sed -i -e "s/{Location}/"`echo $line | cut -d ',' -f 3`", "`echo $line | cut -d ',' -f 4`"/g" tmp.svg
+    sed -i -e "s/{PositionAtEvent}/"`echo $line | cut -d ',' -f 5`"/g" tmp.svg
+    rsvg-convert -f pdf -o "badges2/ohbm_brainhack_2018_badge_front_"`echo $line | cut -d ',' -f 2``echo $line | cut -d ',' -f 1`".pdf" tmp.svg
 done
